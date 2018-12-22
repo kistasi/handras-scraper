ac3047d1512f752d57671831d84d0248d1ee8777
Marton Tasnadi <marton.tasnadi@gmail.com>
2018-12-22 19:50:39 +0100

    Very first unit test

---
+++ handras_test.py
@@ -0,0 +1,7 @@
+import unittest
+import handras
+
+class HandrasTest(unittest.TestCase):
+    def test_get_url(self):
+        for number in range(1, 34):
+            self.assertEqual("http://handras.hu/page/" + str(number), handras.get_url(number))
